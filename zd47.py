2acb38df15c1d9195ac24c77fa226fb3c40342bd
Kirpich-12 <ponchik34@tuta.io>
2024-10-15 17:00:20 +0300

    lsrt

--- zd47.py
+++ zd47.py
@@ -1,7 +1,11 @@
-a = 239
+input_data = open('input.txt', 'r')
+a = int(input_data.read())
+
+
 Del = []
 count = 0
 Max = 0
+max_ch = 0
 
 for i in range(1,a + 1):
     if a % i == 0:
@@ -9,9 +13,17 @@ for i in range(1,a + 1):
 
 for i in range(len(Del)):
     for j in range(len(Del[i])):
-        count += int(j)
-    if count > Max:
+        count += int((Del[i])[j])
+    if count > max_ch:
         Max = Del[i]
+        max_ch = count
         count = 0
+        
 
-print(Max)
+output_data = open('output.txt', 'w')
+output_data.write(str(Max))
+#запись
+
+input_data.close()
+output_data.close()
+#ОБЯЗАТЕЛЬНО не забывать
